b632075e30eaaaaf42f721224bc481de653d44cd
chaosir <qq278935403@gmail.com>
2020-08-08 17:15:11 +0800

    login error notice

--- src/utils/request.js
+++ src/utils/request.js
@@ -47,11 +47,16 @@ service.interceptors.response.use(
     const res = response.data
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 200) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
+      if (res.code === 401) {
+        removeToken()
+        router.push('/login')
+      } else {
+        Message({
+          message: res.message || 'Error',
+          type: 'error',
+          duration: 5 * 1000
+        })
+      }
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
@@ -66,10 +71,7 @@ service.interceptors.response.use(
           })
         })
       }
-      if (res.code === 401) {
-        removeToken()
-        router.push('/login')
-      }
+
       console.log()
       return new Error(res.message || 'Error')
     } else {
